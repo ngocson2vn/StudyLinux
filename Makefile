3dbc9825b77f7b539b3a5e5b386fd4be643b083d
Son Nguyen <ngocson2vn@gmail.com>
2014-07-19 14:47:13 +0900

    Add eps command

--- Makefile
+++ Makefile
@@ -2,6 +2,8 @@ all: select.c
 	@gcc -o select select.c
 	@exec ./select
 
-s2: select2.c
-	gcc -o s2 select2.c
-	exec ./s2
+s2: FORCE
+	@gcc -o s2 select2.c
+	@./s2
+
+FORCE:
--- select2.c
+++ select2.c
@@ -6,100 +6,86 @@
 #include <string.h>
 #include <time.h>
 
-#define TIMEOUT 5		// select timeout in seconds
+#define TIMEOUT 10		// select timeout in seconds
 #define BUF_LEN 1024	// read buffer in bytes
 
 int main(void) {
 	struct timeval tv;
 	fd_set readfds;
-	fd_set writefds;
 	int ret;
 	int n;
+
+	printf("PID = %d\n", getpid());
+
 	const char* file_path = "/tmp/select26061985.txt";
 	
-	int fd_w = open(file_path, O_CREAT | O_WRONLY);
-	FD_ZERO(&writefds);
+	int fd_r = open(file_path, O_RDONLY);
 
-	if (fd_w > 0) {
-		printf("open fd = %d\n", fd_w);
-		FD_SET(fd_w, &writefds);
+	if (fd_r > 0) {
+		printf("open fd = %d\n", fd_r);
 	} else {
 		perror("open");
-	}
-
-	n = (fd_w > 0) ? fd_w + 1 : STDIN_FILENO + 1;
-
-	// Wait on stdin for input.
-	FD_ZERO(&readfds);
-	FD_SET(STDIN_FILENO, &readfds);
-
-	// Wait up to five seconds.
-	tv.tv_sec = TIMEOUT;
-	tv.tv_usec = 0;
-
-	// All right, now block!
-	ret = select(n, &readfds, &writefds, NULL, &tv);
-
-	if (ret == -1) {
-		perror("select");
 		return 1;
-	} else if (!ret) {
-		printf("%d seconds elapsed.\n", TIMEOUT);
-		return 0;
 	}
 
-	printf("remain %d seconds\n", tv.tv_sec);
+	while (1) {
+		FD_ZERO(&readfds);
+		
+		FD_SET(fd_r, &readfds);
 
-	/*
-	 * Is your file descriptor ready to read?
-	 * (It must be, as it was the only fd that
-	 * we provided and the call returned
-	 * nonzero, but we will humor ourselves.)
-	 */
-	if (FD_ISSET(STDIN_FILENO, &readfds)) {
-		printf("fd %d is ready to read\n", STDIN_FILENO);
+		// Wait on stdin for input.
+		FD_SET(STDIN_FILENO, &readfds);
+		
+		n = fd_r + 1;
 
-		char buf[BUF_LEN + 1];
-		int len;
+		// Wait up to TIMEOUT seconds.
+		tv.tv_sec = TIMEOUT;
+		tv.tv_usec = 0;
 
-		// guaranteed to not block
-		len = read(STDIN_FILENO, buf, BUF_LEN);
-		if (len == -1) {
-			perror("read");
+		// All right, now block!
+		ret = select(n, &readfds, NULL, NULL, &tv);
+
+		if (ret == -1) {
+			perror("select");
 			return 1;
+		} else if (!ret) {
+			printf("%d seconds elapsed.\n", TIMEOUT);
+			return 0;
 		}
 
-		if (len) {
-			buf[len] = '\0';
-			printf("read: %s\n", buf);
+		printf("remain %d seconds\n", tv.tv_sec);
+
+		/*
+		 * Is your file descriptor ready to read?
+		 * (It must be, as it was the only fd that
+		 * we provided and the call returned
+		 * nonzero, but we will humor ourselves.)
+		 */
+		int fd = 0;
+		for (fd; fd < n; fd++) {
+			if (FD_ISSET(fd, &readfds)) {
+				char buf[BUF_LEN + 1];
+				int len;
+				printf("fd %d is ready to read\n", fd);
+
+				// guaranteed to not block
+				while ((len = read(fd, buf, BUF_LEN)) > 0) {
+					buf[len] = '\0';
+					printf("read: %s\n", buf);
+					if (fd == STDIN_FILENO) {
+						break;
+					}
+				}
+
+				if (len == 0) {
+					printf("fd %d reached EOF\n", fd);
+				}
+
+				printf("\n");
+			}
 		}
-	}
 
-	if (FD_ISSET(fd_w, &writefds)) {
-		printf("fd %d is ready to write\n", fd_w);
-
-		int ret_w;
-		time_t t = time(NULL);
-		struct tm tm = *localtime(&t);
-		FILE* stream = fdopen(fd_w, "w");
-
-		ret_w = fprintf(stream, "%d-%d-%d %d:%d:%d My name is Son\n",
-				tm.tm_year + 1900,
-				tm.tm_mon + 1,
-				tm.tm_mday, 
-				tm.tm_hour,
-				tm.tm_min,
-				tm.tm_sec);
-
-		if (ret_w == -1) {
-			perror("fprintf");
-			fclose(stream);
-			close(fd_w);
-			return 1;
-		}
-
-		fclose(stream);
-		close(fd_w);
+		sleep(2);
 	}
 
 	return 0;
